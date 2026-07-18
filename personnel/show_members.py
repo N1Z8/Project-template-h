19fa6b5439f2cbdf49dd74db03f8ec4c008c4c31
Buckle27199 <304106853+Buckle21799@users.noreply.github.com>
2026-07-18 11:48:26 +0700

    show members naja

--- personnel/show_members.py
+++ personnel/show_members.py
@@ -1,12 +1,18 @@
 # =====================================================
-#  personnel/show_members.py — คนรับผิดชอบ: ______________________
+#  personnel/show_members.py — คนรับผิดชอบ: PPP
 # =====================================================
 from data import family_members
 
 # TODO: สร้างฟังก์ชัน show_members()
 #   - print ข้อมูลลูกน้องทุกคนใน family_members บรรทัดละคน (ชื่อ, ตำแหน่ง, ความโหด, อาวุธ)
 
+def show_members():
+    for mem in family_members:
+        print(mem)
 
 # ทดสอบ: python -m personnel.show_members
 if __name__ == "__main__":
     show_members()   # ต้องเห็น Tony กับ Luigi คนละบรรทัด
+
+
+
